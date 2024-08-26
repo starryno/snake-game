85fcd953eba576efef9c978809bfb5f374b8bec6
starryno <rayyansmkpi@gmail.com>
2024-08-26 16:23:10 +0800

    Update input.js

--- input.js
+++ input.js
@@ -1,23 +1,23 @@
 let inputDirection = { x: 0, y: 0 }
-let LastInputDirection = { x: 0, y: 0 }
+let lastInputDirection = { x: 0, y: 0 }
 
 window.addEventListener('keydown', e => {
   switch (e.key) {
     case 'ArrowUp':
       if (lastInputDirection.y !== 0) break
-      inputDirection = {x: 0, y: -1 }
+      inputDirection = { x: 0, y: -1 }
       break
-    case 'ArrowDown":
+    case 'ArrowDown':
       if (lastInputDirection.y !== 0) break
-      inputDirection = { x: -1, y: 0 }
+      inputDirection = { x: 0, y: 1 }
       break
     case 'ArrowLeft':
-      if (lastInputDirection.x 1!== 0) break
+      if (lastInputDirection.x !== 0) break
       inputDirection = { x: -1, y: 0 }
       break
     case 'ArrowRight':
       if (lastInputDirection.x !== 0) break
-      inputDirection + { x: 1, y: 0 }
+      inputDirection = { x: 1, y: 0 }
       break
   }
 })
